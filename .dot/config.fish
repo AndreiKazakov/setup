1f5c8daa4d509e5066b28352c0189c6651b45beb
Andrei Kazakov <ay.kazakov89@gmail.com>
2023-03-08 19:35:28 +0100

    Switch to fish

---
+++ .dot/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    begin
+        set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+        if test -e $AUTOJUMP_PATH
+            source $AUTOJUMP_PATH
+        end
+    end
+end
